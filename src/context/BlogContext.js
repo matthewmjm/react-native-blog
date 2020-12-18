91ec8eeba6042bcda3d9dfab24058ab2dc04b4ec
matthewmjm <matthewmjm@yahoo.com>
2020-12-17 22:01:53 -0700

    Move data with Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,5 +5,7 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
 
-    return <BlogContext.Provider>{children}</BlogContext.Provider>;
+    return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
 };
+
+export default BlogContext;

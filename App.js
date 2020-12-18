c70857a8083380bba7486a78485d8692ccc44dc7
matthewmjm <matthewmjm@yahoo.com>
2020-12-17 21:51:18 -0700

    wire-up Context

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import React from 'react';
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
@@ -14,4 +16,13 @@ const navigator = createStackNavigator(
   }
 )
 
-export default createAppContainer(navigator)
+const App = createAppContainer(navigator)
+
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};

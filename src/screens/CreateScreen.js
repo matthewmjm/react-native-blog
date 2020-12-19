730e4aab6723f47e40815fa6385e05cb28bf00eb
matthewmjm <matthewmjm@yahoo.com>
2020-12-18 21:08:17 -0700

    Create New Blog functionality

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,6 +5,7 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = ({ navigation }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(Context);
     return (
         <View>
             <Text style={styles.label}>Enter Title:</Text>
@@ -23,7 +24,10 @@ const CreateScreen = ({ navigation }) => {
                 value={content} 
                 onChangeText={(text) => setContent(text)} 
             />
-            <Button title="Add Blog Post" />
+            <Button 
+                onPress={() => addBlogPost(title, content)}
+                title="Add Blog Post" 
+            />
         </View>
     );
 };

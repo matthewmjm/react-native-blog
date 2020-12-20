85374d07c34df5149f282a07f1f6e71004891aa0
matthewmjm <matthewmjm@yahoo.com>
2020-12-20 11:04:45 -0700

    extracted form logic into it's own componenet

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, {useContext } from 'react';
+import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
-
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
     const { state } = useContext(Context);
@@ -10,18 +10,9 @@ const EditScreen = ({ navigation }) => {
         blogPost => blogPost.id === navigation.getParam('id')
     );
 
-    const [title, setTitle] = useState(blogPost.title)
-    const [content, setContent] = useState(blogPost.content)
-
-
     return (
-        <View>
-            <Text>BLOG POST: #{navigation.getParam('id')}</Text>
-            <Text>Edit Title:</Text>
-            <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)}></TextInput>
-            <Text>Edit Content:</Text>
-            <TextInput value={content} onChangeText={(newContent) => setContent(newContent)}></TextInput>
-        </View>
+        <BlogPostForm>
+        </BlogPostForm>
     );
 };
 

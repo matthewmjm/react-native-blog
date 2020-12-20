b6a3835d93a33b2dee97d7599a13fbf70eaaac6f
matthewmjm <matthewmjm@yahoo.com>
2020-12-20 11:19:57 -0700

    use state to autofill title & content fields in the Edit Screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -11,8 +11,12 @@ const EditScreen = ({ navigation }) => {
     );
 
     return (
-        <BlogPostForm>
-        </BlogPostForm>
+        <BlogPostForm 
+            initialValues={{title: blogPost.title, content: blogPost.content}}
+            onSubmit={(title, content) => {
+                console.log(title, content);
+            }} 
+        />
     );
 };
 
